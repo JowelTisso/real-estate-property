8a6d84121697db77ace3c769d9075c4a4f34ac25
Jowel Tisso <52632590+JowelTisso@users.noreply.github.com>
2022-08-24 18:19:32 +0530

    feat: basic header added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+import React from "react";
+import logo from "../../assets/favicon.png";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img className="logo" src={logo} alt="logo" />
+      <h1 className="app-title">Real Estate Property</h1>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,18 +2,24 @@ import "./Home.css";
 import React, { useEffect } from "react";
 import { GET } from "../../utils/axios";
 import { API } from "../../utils/Constant";
+import Header from "../../components/Header/Header";
 
 const Home = () => {
   useEffect(() => {
-    (async () => {
-      const res = await GET(
-        `${API.PROPERTY_LIST}/?locationExternalIDs=5002,6020&purpose=for-sale&hitsPerPage=6`
-      );
-      console.log(res);
-    })();
+    // (async () => {
+    //   const res = await GET(
+    //     `${API.PROPERTY_LIST}/?locationExternalIDs=5002,6020&purpose=for-sale&hitsPerPage=6`
+    //   );
+    //   console.log(res);
+    // })();
   }, []);
 
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      <main className="content-wrapper"></main>
+    </div>
+  );
 };
 
 export default Home;
